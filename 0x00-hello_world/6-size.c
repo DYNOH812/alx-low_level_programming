17587273cfeba96aae5bb02ba0e240f302e17be3
DYNOH812 <you@example.com>
2023-02-16 14:50:10 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-printf("size of char is %zu byte(s)\n", sizeof(char));
-printf("size of int is %zu byte(s)\n", sizeof(int));
-printf("size of long int is %zu byte(s)\n", sizeof(long int));
-printf("size of long long int is %zu byte(s)\n", sizeof(long long int));
-printf("size of float %zu byte(s)\n", sizeof(float));
+printf("size of char is %lu byte(s)\n", sizeof(char));
+printf("size of int is %lu byte(s)\n", sizeof(int));
+printf("size of long int is %lu byte(s)\n", sizeof(long int));
+printf("size of long long int is %lu byte(s)\n", sizeof(long long int));
+printf("size of float %lu byte(s)\n", sizeof(float));
 return (0);
 }
 		
