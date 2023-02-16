94c31e67d652407fdd2094f555db8856dfbdba0a
DYNOH812 <you@example.com>
2023-02-16 14:34:54 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
-*main -Entry point
+*main -program that prints the size of various types on the computer it is compiled and run on.
 *Return always 0 (success)
 */
 int main(void)
