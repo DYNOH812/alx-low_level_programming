3095dbcd51dea9c0178a50764564b0b039202056
DYNOH812 <you@example.com>
2023-02-16 13:56:37 -0800

    ubuntu

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unitsd>
+/*main Entry
+*Return: Always 1
+*/
+int main(void)
+{
+printf(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
+}
