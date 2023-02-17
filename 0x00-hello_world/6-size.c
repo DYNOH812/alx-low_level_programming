26c78e656a68e8773ea24426a3ec882be6fe15f1
DYNOH812 <you@example.com>
2023-02-16 22:12:45 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of char is %lu byte(s)\n",(unsigned long)Sizeof(a));
-printf("Size of int is %lu byte(s)\n",(unsigned long)Sizeof(b));
-printf("Size of long int is %lu byte(s)\n", (unsigned long)Sizeof(c));
-printf("Size of long long int is %lu byte(s)\n",(unsigned long)Sizeof(d);
-printf("Size of float %lu byte(s)\n",(unsigned long)Sizeof(f));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d);
+printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
