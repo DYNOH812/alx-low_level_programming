bc16ac8c70482c1cab2fe62f2b2f6282a6d26d73
DYNOH812 <you@example.com>
2023-02-20 11:58:41 -0800

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+
 /**
 *main -print out sizes of data in c
-* Return:0
+*
+* Return:0 on success
 */
+
 int main(void)
 {
-printf("Size of  char: %lu bytes(s)\n", sizeof(char));
-printf("Size of  int: %lu bytes(s)\n", sizeof(int));
-printf("Size of  long int: %lu bytes(s)\n", sizeof(long int));
-printf("Size of  long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of  char: %lu bytes(s)", sizeof(char));
+printf("Size of  int: %lu bytes(s)", sizeof(int));
+printf("Size of  long int: %lu bytes(s)", sizeof(long int));
+printf("Size of  long long int: %lu bytes(s)", sizeof(long long int));
 printf("Size of float: %lu bytes(s)", sizeof(float));
 return (0);
 }
